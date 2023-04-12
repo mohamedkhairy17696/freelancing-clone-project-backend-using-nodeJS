99b2a03a86b576cf15fb1c3a42dfd96646b0c558
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-12 09:37:29 +0200

    login route handling without token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,7 +15,22 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("From Auth Controller");
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) {
+      return res.status(404).send("Wrong password or username");
+    }
+
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
 };
 
 export const logout = async (req, res) => {
