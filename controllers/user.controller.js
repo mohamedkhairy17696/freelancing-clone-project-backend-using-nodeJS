d1b70ee99a5ff38017c106e2bca4ea9a102bd283
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-02 01:58:36 +0300

    commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,11 +15,6 @@ export const deleteUser = async (req, res, next) => {
 export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
-    if (!mongoose.Types.ObjectId.isValid(id)) return false;
-
-    if (!user) {
-      next(createError(404, "user not found!"));
-    }
     res.status(200).send(user);
   } catch (err) {
     next(err);
