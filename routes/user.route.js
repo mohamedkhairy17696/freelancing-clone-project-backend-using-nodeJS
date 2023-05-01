c2369320c06b3a50e12b7b5bea73ca9b7d0e029a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-02 01:38:47 +0300

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,7 +8,7 @@ import { verifyToken } from "../middleware/jwt.js";
 const router = express.Router();
 
 router.delete("/:id", deleteUser);
-router.get("/:id", getUser);
+router.get("/:id", verifyToken, getUser);
 router.get("/", getUsers);
 
 export default router;
