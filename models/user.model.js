7db75966d13827372ce4020d2ebe7975b15e1dd6
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-02 18:31:31 +0300

    commit

--- models/user.model.js
+++ models/user.model.js
@@ -7,11 +7,13 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
+      required: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      required: true,
     },
     password: {
       type: String,
