52cbc4ddeccb9c845749787224dedffd87febb5a
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-05-04 19:07:55 +0300

    commit

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
